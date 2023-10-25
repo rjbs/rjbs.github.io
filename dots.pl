688a35ad5a246b1c2437f5dbfe018a3490713fdd
Ricardo Signes <rjbs@semiotic.systems>
2023-10-24 22:01:35 -0400

    dots.pl: remove work stuff, which can now live elsewhere

--- dots.pl
+++ dots.pl
@@ -9,24 +9,10 @@ my @repos = qw(
 
 my $home = $ENV{HOME};
 
-my $IS_FM_VM    = -f '/etc/fmisproduction.boxdc';
-my $START_OVER  = $IS_FM_VM;
-my $UPDATE_ROOT = $IS_FM_VM;
-
 chdir($home) || die "can't chdir to $home: $!\n";
 
 unless (-d "dots") {
   mkdir("dots") || die "can't mkdir dots: $!\n";
-}
-
-if ($START_OVER) {
-  unlink("$home/.bashrc") || warn "couldn't unlink ~/.bashrc: $!";
-
-  if ($UPDATE_ROOT) {
-    for my $file (qw( /root/.bashrc /root/.vimrc /root/.gitconfig )) {
-      system('sudo', 'rm', '-f', $file) && warn "couldn't unlink $file: $!";
-    }
-  }
 }
 
 for my $repo (@repos) {
@@ -46,15 +32,4 @@ for my $repo (@repos) {
 
   system("$home/dots/rjbs-dots/bin/link-install", "--really");
   die "error installing $repo\n" if $?;
-
-  if ($UPDATE_ROOT) {
-    system('sudo', "$home/dots/rjbs-dots/bin/link-install", '--really');
-    warn "error installing $repo to ~root\n" if $?;
-  }
-}
-
-if ($IS_FM_VM) {
-  for my $file (qw( /root/.ssh/config )) {
-    system('sudo', 'rm', '-f', $file) && warn "couldn't unlink $file: $!";
-  }
 }
